0c4d69b03619438c57890654d7f1442ef72321cc
Prathzz <80695458+Prathzz@users.noreply.github.com>
2021-04-15 23:33:43 +0530

    Delete gotoxy.c

--- gotoxy.c
+++
@@ -1,8 +0,0 @@
-#include "record.h"
-COORD coord = {0,0};
-void gotoxy(int xcoordinate,int ycoordinate)
-{
-    coord.X = xcoordinate;
-    coord.Y = ycoordinate;
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),coord);
-}
