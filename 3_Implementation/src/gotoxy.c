5f44e947c422b9f646b8dccdb0753556f20a2236
Prathzz <80695458+Prathzz@users.noreply.github.com>
2021-04-15 23:28:14 +0530

    Update gotoxy.c

--- 3_Implementation/src/gotoxy.c
+++ 3_Implementation/src/gotoxy.c
@@ -1,1 +1,9 @@
 
+#include "record.h"
+COORD coord = {0,0};
+void gotoxy(int xcoordinate,int ycoordinate)
+{
+    coord.X = xcoordinate;
+    coord.Y = ycoordinate;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),coord);
+}
