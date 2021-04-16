286e7503ce07178276d6aec9ecf2922b867a17c8
Prathzz <80695458+Prathzz@users.noreply.github.com>
2021-04-16 16:24:37 +0530

    Update test_employee.c

--- 3_Implementation/test/test_employee.c
+++ 3_Implementation/test/test_employee.c
@@ -1,1 +1,32 @@
 
+#include "unity.h"
+#include <record.h>
+
+#define PROJECT_NAME "employee"
+
+/*function prototype*/
+void gotoxytest(int xcoordinate,int ycoordinate);
+
+/* Required by the unity test framework */
+void setUp(){}
+/* Required by the unity test framework */
+void tearDown(){}
+
+int main()
+{
+/* Initiate the Unity Test Framework */
+  UNITY_BEGIN();
+
+/* Run Test functions */
+  RUN_TEST(gotoxytest);
+  
+  return UNITY_END();
+}
+
+
+void gotoxytest(void){
+  TEST_ASSERT_EQUAL(0,gotoxy(30,20));
+  TEST_ASSERT_EQUAL(0,gotoxy(30,22));
+}
+
+
